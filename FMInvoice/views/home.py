0ad129af855499b021dd849189686c5906e292dc
Pegasus03 <estimejesus@gmail.com>
2025-02-27 20:31:32 +0200

    Just prepare some basic file (for crud operation) update home page index.html

--- FMInvoice/views/home.py
+++ FMInvoice/views/home.py
@@ -1,16 +1,34 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from FMInvoice.models import Emission, Service, UtilisateurEmetteurStation, Facture, StationRadioDepartement, \
+    CategorieEmissionService
 
 
 @login_required
-# Function to view home page
 def index(request):
     assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'FMInvoice/home/index.html'
-    )
+
+    # Récupération des statistiques dynamiques
+    total_emissions = Emission.objects.count()
+    total_services = Service.objects.count()
+    total_utilisateurs = UtilisateurEmetteurStation.objects.count()
+    total_stations = StationRadioDepartement.objects.count()
+    total_factures = Facture.objects.filter(statut_validite='Valide').count()
+    total_categories = CategorieEmissionService.objects.count()
+
+    # Contexte à passer au template
+    context = {
+        'total_emissions': total_emissions,
+        'total_services': total_services,
+        'total_utilisateurs': total_utilisateurs,
+        'total_stations': total_stations,
+        'total_factures': total_factures,
+        'total_categories': total_categories,
+        'user': request.user,
+    }
+
+    return render(request, 'FMInvoice/home/index.html', context)
 
 
 # Display a view for after redirecting the new employee without is_staff or is_superuser roles
